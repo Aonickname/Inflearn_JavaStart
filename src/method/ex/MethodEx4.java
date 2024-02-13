569b029763011149c3b571e087d4275979052599
sebins <bysws302@naver.com>
2024-02-14 01:16:53 +0900

    메서드 문제와 풀이2

--- src/method/ex/MethodEx4.java
+++ src/method/ex/MethodEx4.java
@@ -5,60 +5,63 @@ import java.util.Scanner;
 public class MethodEx4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int choice = 0;
         int balance = 0;
-        int amount = 0;
 
         while (true) {
-
-            System.out.println("-----------------------------------");
+            System.out.println("---------------------------------");
             System.out.println("1.입금 | 2.출금 | 3.잔액 확인 | 4.종료");
-            System.out.println("-----------------------------------");
+            System.out.println("---------------------------------");
             System.out.print("선택: ");
+            int choice = scanner.nextInt();
+            int amount;
 
-            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("입금액을 입력하세요: ");
+                    amount = scanner.nextInt();
+                    balance = deposit(balance, amount);
+                    break;
 
-            if (choice == 1) {
-                System.out.print("입금액을 입력하세요: ");
-                amount = scanner.nextInt();
-                balance = deposit(balance, amount);
+                case 2:
+                    System.out.print("출금액을 입력하세요: ");
+                    amount = scanner.nextInt();
+                    balance = withdraw(balance, amount);
+                    break;
 
-            } else if (choice == 2) {
-                System.out.print("출금액을 입력하세요: ");
-                amount = scanner.nextInt();
-                balance = withdraw(balance, amount);
+                case 3:
+                    System.out.println("현재 잔액: " + balance + "원");
+                    break;
 
-            } else if (choice == 3) {
-                System.out.println("현재 잔액: " + balance + "원");
-                
-            } else if (choice == 4) {
-                System.out.println("시스템을 종료합니다.");
-                break;
-                
-            } else {
-                System.out.println("올바른 선택이 아닙니다. 다시 선택해주세요.");
+                case 4:
+                    System.out.println("시스템을 종료합니다.");
+                    return;
+
+                default:
+                    System.out.println("올바른 선택이 아닙니다. 다시 선택해주세요.");
             }
-
         }
     }
+
+
 
     public static int deposit(int balance, int amount) {
         balance += amount;
-        System.out.println(amount + "원을 입금하였습니다. 현재 잔액: " + balance + "원");
+        System.out.println(amount + "원을 입금하였습니다. 현재 잔액: " + balance +
+                "원");
 
         return balance;
     }
-
     public static int withdraw(int balance, int amount) {
-
         if (balance >= amount) {
             balance -= amount;
-            System.out.println(amount + "원 출금하였습니다. 현재 잔액: " + balance + "원");
-        } else if (balance < amount) {
-            System.out.println(amount + "원을 출금하려 했으나 잔액이 부족합니다.");
+            System.out.println(amount + "원을 출금하였습니다. 현재 잔액: " + balance + "원");
+
+        }   else {
+                System.out.println(amount + "원을 출금하려 했으나 잔액이 부족합니다.");
+
         }
 
-        return balance;
+          return balance;
+        }
     }
 
-}
