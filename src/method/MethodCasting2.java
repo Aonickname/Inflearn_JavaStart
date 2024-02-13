daeb9ec8ad57f442391ac05e246b8d62f60d0629
sebins <bysws302@naver.com>
2024-02-14 00:02:57 +0900

    메서드와 형변환

---
+++ src/method/MethodCasting2.java
@@ -0,0 +1,14 @@
+package method;
+
+public class MethodCasting2 {
+    public static void main(String[] args) {
+        int number = 100;
+        printNumber(number); //파라미터, 인수
+
+    }
+
+    public static void printNumber(double n) { //매개변수
+        System.out.println("숫자: " + n);
+
+    }
+}
